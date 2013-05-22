17eb4f14a2a71a280938205f28c4b2a23d71a18e
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-05-22 20:57:08 +0200

    URLs in links corrected

--- app.rb
+++ app.rb
@@ -14,10 +14,22 @@ configure do
   set :db, Redis.new(YAML.load_file('db.yml'))
 end
 
+configure :development do
+  Slim::Engine.default_options[:pretty] = true
+end
+
 configure :test do
   disable :logging
 end
 
+get '/' do
+  redirect to('/designer')
+end
+
 get '/designer' do
   slim :designer_index, :layout => :layout_designer
+end
+
+get %r{/designer/(synonyms|homophones|figures)} do |m|
+  slim "designer_#{m}".to_sym, :layout => :layout_designer
 end
