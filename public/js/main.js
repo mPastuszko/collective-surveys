0a32b562e6fbef17a43d6e18fd5234b43126cb42
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-05-22 22:33:13 +0200

    Pane activation by URL anchor added

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
+function activateTabByAnchor() {
+  var anchor = window.location.hash.substring(1);
+  if(anchor) {
+    $('.nav-tabs a[href="#' + anchor + '-pane"]').click();
+  }
+}
 
 $(function() {
-  
+  activateTabByAnchor();
 });
