11bd729ed68f516aea3cacb36e7e3e40e463b82d
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-05-24 18:06:15 +0200

    Form validation refactored and extended with radio inputs support

--- public/js/main.js
+++ public/js/main.js
@@ -7,14 +7,34 @@ function activateTabByAnchor() {
 
 function validateQuestionsForm() {
   var form = $(this).parents('form.questions');
-  var valid = form.find('input.answer').filter(function() {
-    return $(this).val() === '';
-  }).size() === 0;
+  var valid = validateForm(form);
 
   form.find('button.continue').prop('disabled', !valid);
 }
 
+function validateForm(form) {
+  return validateTextInputs(form) &&
+         validateRadioInputs(form);
+}
+
+function validateTextInputs(form) {
+  return form.find('input.answer[type="text"]').filter(function() {
+    return $(this).val() === '';
+  }).size() === 0;
+}
+
+function validateRadioInputs(form) {
+  return form.find('input.answer[type="radio"]').filter(function() {
+    var group = $(this).attr('name');
+    return form.find('input.answer[type="radio"][name="' + group + '"]').filter(function() {
+      return $(this).prop('checked');
+    }).size() === 0;
+  }).size() === 0;
+}
+
 $(function() {
   activateTabByAnchor();
-  $('form.questions input.answer').keyup(validateQuestionsForm);
+  $('form.questions input.answer').
+    keyup(validateQuestionsForm).
+    change(validateQuestionsForm);
 });
