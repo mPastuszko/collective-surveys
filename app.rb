a559081cfa63bbc527d879355197dc240211e14b
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-05-25 15:52:04 +0200

    Little correction in error message

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'redis'
 
 configure do
   enable :sessions
-  raise 'Session secret key not fond. Run `rake generate_session_secret` to generate one.' \
+  raise 'Session secret key not fond. Run `rake session.secret` to generate one.' \
     unless File.exists?('session.secret')
   set :session_secret, File.read('session.secret')
   set :db, Redis.new(YAML.load_file('db.yml'))
